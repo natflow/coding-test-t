17bff2d5d21decb4a50a35a1601b64ee2c6b6777
Nat Flowers <791900+natflow@users.noreply.github.com>
2021-02-05 16:03:13 -0800

    rewrite models and operations for movies

--- app/models.py
+++ app/models.py
@@ -1,26 +1,20 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, DateTime
 
 from .database import Base
 
 
-class User(Base):
-    __tablename__ = "users"
+class Movie(Base):
+    __tablename__ = "movies"
 
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
-
-    items = relationship("Item", back_populates="owner")
-
-
-class Item(Base):
-    __tablename__ = "items"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("users.id"))
-
-    owner = relationship("User", back_populates="items")
+    show_id = Column(String, primary_key=True, index=True)
+    #type = Column(String) # always "Movie"
+    title = Column(String)
+    director = Column(String)
+    cast = Column(String)
+    country = Column(String)
+    date_added = Column(DateTime)
+    release_year = Column(Integer)
+    rating = Column(String)
+    duration = Column(String)
+    listed_in = Column(String)
+    description = Column(String)
--- app/schemas.py
+++ app/schemas.py
@@ -1,76 +1,22 @@
-from typing import List, Optional
+from datetime import date
 
 from pydantic import BaseModel
-from sqlalchemy.orm import Session
-
-from . import models
 
 
-class ItemBase(BaseModel):
+class MovieSchema(BaseModel):
+    # TODO: maybe add strictness to types
+    show_id: str
+    # type: str = Literal("Movie")
     title: str
-    description: Optional[str] = None
-
-
-class ItemCreate(ItemBase):
-    pass
-
-
-class Item(ItemBase):
-    id: int
-    owner_id: int
-
-    class Config:
-        orm_mode = True
-
-
-class UserBase(BaseModel):
-    email: str
-
-
-class UserCreate(UserBase):
-    password: str
-
-
-class User(UserBase):
-    id: int
-    is_active: bool
-    items: List[Item] = []
+    director: str
+    cast: str
+    country: str
+    date_added: date # TODO: parsing/formatting; pendulum.parse("April 1, 2019", strict=False)
+    release_year: str
+    rating: str
+    duration: str
+    listed_in: str
+    description: str
 
     class Config:
         orm_mode = True
-
-
-######
-
-
-def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id == user_id).first()
-
-
-def get_user_by_email(db: Session, email: str):
-    return db.query(models.User).filter(models.User.email == email).first()
-
-
-def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.User).offset(skip).limit(limit).all()
-
-
-def create_user(db: Session, user: UserCreate):
-    fake_hashed_password = user.password + "notreallyhashed"
-    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
-
-
-def get_items(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Item).offset(skip).limit(limit).all()
-
-
-def create_user_item(db: Session, item: ItemCreate, user_id: int):
-    db_item = models.Item(**item.dict(), owner_id=user_id)
-    db.add(db_item)
-    db.commit()
-    db.refresh(db_item)
-    return db_item
