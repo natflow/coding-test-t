34e2e1758e53646f3f401f0849968ff3e6624024
Nat Flowers <791900+natflow@users.noreply.github.com>
2021-02-19 18:20:48 -0800

    Use root/base paths when specified

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
+import os
+
 from fastapi import FastAPI
 from mangum import Mangum
 
 from .api import router
 
 
-app = FastAPI()
+app = FastAPI(root_path=os.environ.get("FASTAPI_ROOT_PATH"))
 app.include_router(router)
 
-handler = Mangum(app)
+handler = Mangum(app, api_gateway_base_path=os.environ.get("MANGUM_BASE_PATH"))
